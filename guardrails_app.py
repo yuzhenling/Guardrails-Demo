240eb1ef8e1cbb68df7d03daf556cacbce8aca63
yuzhenling <yuzhenling@yeah.net>
2026-04-19 11:38:30 +0800

    0419 API /v1/chat/completions

--- guardrails_app.py
+++ guardrails_app.py
@@ -9,7 +9,7 @@ def main() -> None:
 
     config = RailsConfig.from_path("./config")
 
-    rails = LLMRails(config)
+    rails = LLMRails(config, verbose=True)
 
     tests = [
         "你好呀",
@@ -20,13 +20,25 @@ def main() -> None:
 
     for i, text in enumerate(tests, start=1):
         # text = f"请用中文进行回答这个消息: {text}"
-        resp = rails.generate(messages=[{"role": "user", "content": text}])
+        resp = rails.generate(
+                    messages=[{"role": "user", "content": text}],
+                    options={
+                        "log": {
+                        "activated_rails": True,
+                        "llm_calls": True,
+                        "internal_events": True,
+                        "colang_history": True
+                        }
+                    }
+                )
         info = rails.explain()
         print(f"\n=== Test {i} ===")
         print("User:", text)
         print("Assistant:", resp.response[0]["content"])
         print("\n--- Colang history ---")
         print(info.colang_history)
+        print("\n--- llm_calls_summary ---")
+        print(info.print_llm_calls_summary())
 
 
 if __name__ == "__main__":
